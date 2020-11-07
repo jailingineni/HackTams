0a609f7528fb7f5e69d917bd01f8501707463fce
jailingineni <jailingineni@gmail.com>
2020-11-07 17:07:57 -0600

    adding city code

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+var city1 = {
+    "name":"Dallas",
+    "count":10000
+}
+
+var allCities = [];
+
+allCities.push(city1);
+
 
 function addListValues(){
 
-    for (var i = 0; i < 10; i++) {
-        var title = "my made up title"
-        var subtitle = "my other stuff"
+    for(var l = 0; l< allCities.length; l++){
+
+        var city = allCities[l];
+        var name = city["name"];
+        var count = city["count"]
+        
         var item = `<div class="item">
     <i class="large github middle aligned icon"></i>
                        <div class="content">
-                           <a class="header">${title}</a>
-                           <div class="description">${subtitle}</div>
+                           <a class="header">${name}</a>
+                           <div class="description">${count}</div>
                        </div>
                    </div>`
     
     
         document.getElementById("list-container").insertAdjacentHTML('beforeend',item );
-    
     }
+
     
 }
 
