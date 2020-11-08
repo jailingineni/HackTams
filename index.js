9473a934d79f94df745fd1714d93c2618f9db7c7
Nathaniel Liu <71233101+NatL7499@users.noreply.github.com>
2020-11-07 22:55:26 -0600

    info class

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+
 $('#dropdown').dropdown();
 
-
-var counties = [Austin, Bexar, Brazoria, Collin, Dallas, Harris, Hudspeth, King, Willacy, Zavala];
-var activecases = [6, 54441, 755, 1445, 10476, 27545, 33, 0, 56, 12];
-var deaths = [10, 1427, 188, 199, 1318, 2865, 3, 0, 58, 19];
-var GDP = [1271624, 97527759, 15588622, 599117532, 241375801, 361353488, 149878, 208452, 870911, 945577];
-var population = [30032, 2003554, 374264, 1034730, 2635516, 4713325, 4886, 272, 21538, 11840];
-
+class CovidInfo{
+    constructor(counties, activecases, deaths, GDP, population){
+        this.counties = counties;
+        this.activecases = activecases;
+        this.deaths = deaths;
+        this.GDP = GDP;
+        this.population = population;
+    }
+    }
+    
+    var covidinfos = [new CovidInfo('Austin', 6, 10, 1271624, 30032), 
+                      new CovidInfo('Bexar', 54441, 1427, 97527759, 2003554),
+                      new CovidInfo('Brazoria', 755, 188, 15588622, 374264),
+                      new CovidInfo('Collin', 1445, 199, 599117532, 1034730),
+                      new CovidInfo('Dallas',	10476, 1318, 241375801, 2635516),
+                      new CovidInfo('Harris', 27545, 2865, 361353488, 4713325),
+                      new CovidInfo('Hudspeth', 34, 3, 149878, 4886),
+                      new CovidInfo('King', 0, 0, 208452, 272),
+                      new CovidInfo('Willacy', 56, 58, 870911, 21358),
+                      new CovidInfo('Zavala', 12, 19, 945577, 11840)];
+function sortArray(sortorder){
+    switch(sortorder){
+        case 'Active Case':
+            covidinfos.sort((a, b)=> (a.activecases > b.activecases ? -1:1));
+            break;
+        case 'Deaths':
+            covidinfos.sort((a, b)=> (a.deaths > b.deaths ? -1:1));
+            break;
+        case 'GDP':
+            covidinfos.sort((a, b)=> (a.GDP > b.dGDP ? -1:1));
+            break;
+        case 'Population':
+            covidinfos.sort((a, b)=> (a.population > b.population ? -1:1));
+            break;
+        default: 
+        covidinfos.sort((a, b)=> (a.counties > b.counties ? -1:1));
+        break;
+    }
+}
 
